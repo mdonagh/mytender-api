20a1458685880cb26c6099650a8f62c11b9f69d1
Mark P. Donahue <mark@record360.com>
2023-02-26 15:34:50 -0800

    install active storage

--- spec/factories/shifts.rb
+++ spec/factories/shifts.rb
@@ -1,18 +1,23 @@
 # == Schema Information
 #
-# Table name: users
+# Table name: shifts
 #
-#  id              :bigint           not null, primary key
-#  email           :citext           not null, indexed
-#  first_name      :string
-#  last_name       :string
-#  password_digest :string
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
+#  id         :bigint           not null, primary key
+#  address    :string
+#  duration   :integer
+#  latitude   :decimal(10, 2)   indexed => [longitude, start_time]
+#  longitude  :decimal(10, 2)   indexed => [latitude, start_time]
+#  notes      :string
+#  recurring  :boolean
+#  start_time :datetime         indexed => [latitude, longitude]
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           indexed
 #
 # Indexes
 #
-#  index_users_on_email  (email) UNIQUE
+#  index_shifts_on_latitude_and_longitude_and_start_time  (latitude,longitude,start_time)
+#  index_shifts_on_user_id                                (user_id)
 #
 
 FactoryBot.define do
