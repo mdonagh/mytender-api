84ccc4a6888dd4eed9efa6e39f882609dda65c8f
Mark P. Donahue <mark@record360.com>
2023-08-11 20:19:15 -0700

    newest

--- app/graphql/types/custom_types/shift.rb
+++ app/graphql/types/custom_types/shift.rb
@@ -5,6 +5,8 @@ module Types
       field :user, Types::CustomTypes::User, null: true
       field :notes, String, null: true
       field :bar_name, String, null: true
+      field :distance, Float, null: true
+
       field :address, String, null: true
       field :latitude, String, null: true, method: :lat_as_string
       field :longitude, String, null: true, method: :long_as_string
